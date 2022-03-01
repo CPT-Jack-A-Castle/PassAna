94b94e32c23ca525319ddcbe6ea845fe1249bc4b
ruidonghan <han784851198@live.com>
2022-03-01 15:43:11 +0800

    add findPass and findString for python

--- 1.anaPass.py
+++ 1.anaPass.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     base = '/home/rain/program/java'
     analyze_str(base, 'findPass', skip=True)
     data = merge_csv(base)
-    data.to_csv(f'{base}/string.csv')
+    data.to_csv(f'{base}/pass.csv')
--- 1.anaStr.py
+++ 1.anaStr.py
@@ -2,6 +2,6 @@ from ql.analyzer import analyze_str, merge_csv
 
 if __name__ == '__main__':
     base = '/home/rain/program/java'
-    analyze_str(base, 'findString', skip=True)
+    analyze_str(base, 'findString', skip=False)
     data = merge_csv(base)
     data.to_csv(f'{base}/string.csv')
