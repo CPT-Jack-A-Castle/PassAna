8bc0d779acfcbbd81bae5574a3fbe1881e07174f
ruidonghan <han784851198@live.com>
2022-03-16 14:45:38 +0800

    fix bug about 1.anaPass

--- 1.anaPass.py
+++ 1.anaPass.py
@@ -1,9 +1,9 @@
 from ql.analyzer import init_analyzer
 
 if __name__ == '__main__':
-    language = 'java'
-    base = '/home/rain/program/tmp'
+    language = 'python'
+    base = '/home/rain/program/python'
     analyzer = init_analyzer(language)
-    analyzer.get_str_from_projects(base,threads=8)
+    analyzer.get_pass_from_projects(base,threads=8)
     data = analyzer.merge_csv(base, "findPass")
     data.to_csv(f'{base}/pass.csv')
