c6e2ebe2e1558165d554cb969d60f6a8f3d2f8e5
ruidonghan <han784851198@live.com>
2022-03-16 17:17:31 +0800

    add filter for find pass

--- 1.anaPass.py
+++ 1.anaPass.py
@@ -1,9 +1,11 @@
+from passwd.passTool import process_found_pass
 from ql.analyzer import init_analyzer
 
 if __name__ == '__main__':
-    language = 'java'
-    base = '/home/gonghuihui/pwd_proj/java_database'
+    language = 'python'
+    base = '/home/gonghuihui/pwd_proj/python_database'
     analyzer = init_analyzer(language)
-    # analyzer.get_pass_from_projects(base,threads=8)
+    analyzer.get_pass_from_projects(base,threads=8)
     data = analyzer.merge_csv(base, "findPass")
-    data.to_csv(f'{base}/pass.csv')
+    data.to_csv(f'csv/{language}/pass.csv')
+    process_found_pass(f'csv/{language}')
--- 1.anaStr.py
+++ 1.anaStr.py
@@ -1,8 +1,8 @@
 from ql.analyzer import init_analyzer
 
 if __name__ == '__main__':
-    language = 'java'
-    base = '/home/gonghuihui/pwd_proj/java_database'
+    language = 'python'
+    base = '/home/gonghuihui/pwd_proj/python_database'
     analyzer = init_analyzer(language)
     #analyzer.get_str_from_projects(base, threads=8)
     data = analyzer.merge_csv(base, "findString")
